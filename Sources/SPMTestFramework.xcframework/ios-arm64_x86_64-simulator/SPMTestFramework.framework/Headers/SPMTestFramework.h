e9340357e38b4265903ba3187bb7e41aa33f9ad8
Erge AKOVA <117632508+erge-akova@users.noreply.github.com>
2024-02-28 15:49:01 +0300

    0.0.0.4

---
+++ Sources/SPMTestFramework.xcframework/ios-arm64_x86_64-simulator/SPMTestFramework.framework/Headers/SPMTestFramework.h
@@ -0,0 +1,18 @@
+//
+//  SPMTestFramework.h
+//  SPMTestFramework
+//
+//  Created by Erge AKOVA on 28.02.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SPMTestFramework.
+FOUNDATION_EXPORT double SPMTestFrameworkVersionNumber;
+
+//! Project version string for SPMTestFramework.
+FOUNDATION_EXPORT const unsigned char SPMTestFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SPMTestFramework/PublicHeader.h>
+
+
--- Sources/SPMTestFramework/SPMTest.swift
+++
@@ -1,8 +0,0 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
-
-public struct SPMTest {
-    public static func start() {
-        print("Start SPMTestFramework...")
-    }
-}
