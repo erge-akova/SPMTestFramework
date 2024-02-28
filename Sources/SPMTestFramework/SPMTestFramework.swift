5d1df134050049016cedfb356bbf9ffc9111b72d
Erge AKOVA <117632508+erge-akova@users.noreply.github.com>
2024-02-28 14:22:40 +0300

    0.0.0.1

--- Sources/SPMTestFramework/SPMTestFramework.swift
+++ Sources/SPMTestFramework/SPMTestFramework.swift
@@ -1,2 +1,8 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+public struct SPMTestFramework {
+    public static func start() {
+        print("Start SPMTestFramework...")
+    }
+}
